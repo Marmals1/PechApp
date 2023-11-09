fdab5f91cba4c95b053c573d068cb6a51ca3dd7a
Marmals1 <149095222+Marmals1@users.noreply.github.com>
2023-11-09 12:21:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] people = { "Tina", "Bart", "Sander", "Jan" }; //[] staat voor Array 
+
+Console.WriteLine(people[0]); //eerste element zeg je 0 dus Tina 2e element zeg je 1 dus Bart
+Console.WriteLine(people[people.Length - 1]); //LENGTH GEEFT LENGTE AAN PEUPLE.LENGTH MOET OP -1 OM DE LAATSTE TE PAKKEN.
+
+for (int i = 0; i < people.Length; i++) //i wordt net zo groot als de Array dus 4 personen 
+{
+    Console.WriteLine(people[i]); 
+}
+
+
